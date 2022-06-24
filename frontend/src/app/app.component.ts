ff4d5d011f7f00645894943856f49a896829c170
Caio-eng <caiopereira1998@hotmail.com>
2022-06-24 14:57:40 -0300

    front-end corrigido

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'frontend';
+
+  ngAfterViewInit() {
+    (function($) {
+      "use strict";
+
+      var path = location.pathname;
+
+      // Add active state to sidbar nav links
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index:any, element:any) => {
+        if ($(element).attr('href') === path) {
+            $(element).addClass("active");
+        }
+    });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+
+    })(jQuery);
+  }
 }
