4a9233655487a984071c7d81ac903f750ee68bb1
Caio-eng <caiopereira1998@hotmail.com>
2022-07-11 11:08:22 -0300

    POST

--- frontend/src/app/clientes.service.ts
+++ frontend/src/app/clientes.service.ts
@@ -1,12 +1,19 @@
-import { Cliente } from './clientes/cliente';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Cliente } from './clientes/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  salvar( cliente: Cliente ) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
 
   getCliente(): Cliente {
     let cliente : Cliente = new Cliente();
