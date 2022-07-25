2af67f2519ca405e1fe19d9f7def6e7579b693c1
Caio-eng <caiopereira1998@hotmail.com>
2022-07-25 15:17:55 -0300

    Pesquisar

--- frontend/src/app/servico-prestado.service.ts
+++ frontend/src/app/servico-prestado.service.ts
@@ -1,9 +1,10 @@
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,15 @@ export class ServicoPrestadoService {
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
     return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado);
+  }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]> {
+
+    const httpParams = new HttpParams()
+      .set("nome", nome)
+      .set("mes", mes ? mes.toString() : '');
+
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url);
   }
 }
