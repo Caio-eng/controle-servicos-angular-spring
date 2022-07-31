74a39d6cc8b5744f16732314c8645ed18f9eb378
Caio-eng <caiopereira1998@hotmail.com>
2022-07-31 10:41:11 -0300

    JWT Configurado

--- backend/src/main/java/com/pereira/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/pereira/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pereira.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,15 +11,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.pereira.backend.service.UsuarioService;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("caio")
-            .password("123")
-            .roles("USER");
+        auth
+            .userDetailsService(usuarioService)
+            .passwordEncoder(passwordEncoder());
     }
 
     @Bean
--- backend/src/main/java/com/pereira/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/com/pereira/backend/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.pereira.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pereira.backend.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUsername(String username);
     
 }
